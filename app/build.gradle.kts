43cf74568535929fcbfb2ed61520d64599fd0bf6
Saawry <saawry.hb@gmail.com>
2025-09-10 15:52:00 +0600

    Compose dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,12 +51,16 @@ dependencies {
 
     implementation(platform(libs.androidx.compose.bom)) // Use the BOM for version management
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.ui.tooling)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.ui.graphics)
+
+    debugImplementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
 
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    debugImplementation(libs.androidx.compose.ui.tooling.preview)
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
