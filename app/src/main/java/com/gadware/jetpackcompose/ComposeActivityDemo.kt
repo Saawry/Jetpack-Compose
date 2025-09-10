d74d27fd3e3e9cc0331bf86745f516b5638bebb0
Saawry <saawry.hb@gmail.com>
2025-09-10 18:05:21 +0600

    written composable function that takes data class object

--- app/src/main/java/com/gadware/jetpackcompose/ComposeActivityDemo.kt
+++ app/src/main/java/com/gadware/jetpackcompose/ComposeActivityDemo.kt
@@ -7,24 +7,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
-//extends componentActivity instead of AppCompatActivity
 class ComposeActivityDemo : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContent func will display the content. we have to call composable funcs here to show
         setContent {
-            FirstFunc()
-            SecondFunc("Alhamdulillah")//calling parameterized composable function
+            UserCard(UserInfo("Mohammad","Abdullah"))
         }
     }
 }
 
-@Composable//use annotation to make a func composable
-fun FirstFunc(){
-    Text("Bismillah")
-}
-@Preview//to preview, use annotation. (showSystemUi = true) can be used to show system ui
 @Composable
-fun SecondFunc(data:String="Alhamdulillah"){//there must be a default value of parameters to preview
-    Text("Say $data")
+fun UserCard(user:UserInfo){
+    Text("Name: ${user.name}")
+    Text("Name: ${user.address}")
 }
+
+@Preview
+@Composable
+ fun PreviewUserInfo(){
+    UserCard(user=UserInfo("Mohammad","Abdullah"))
+}
+
+data class UserInfo(val name:String, val address: String)
