eb8e1ff6e216a65b55283994dd4d3d4df917bfba
Saawry <saawry.hb@gmail.com>
2025-09-10 16:40:58 +0600

    written composable function in existing project (xml layout file) and used in activity

--- app/src/main/java/com/gadware/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/gadware/jetpackcompose/MainActivity.kt
@@ -3,18 +3,27 @@ package com.gadware.jetpackcompose
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        /////following code shows how to use composable on existing xml layout file
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //find ComposeViewById which is declared in xml layout
+        val composeView=findViewById<ComposeView>(R.id.compose_view)
+        composeView.setContent {// user setContent method to show ui component or composable function
+            FirstComposableFunc()//calling a composable function
+            Text("Demo Text")// displays  a textview
         }
     }
+}
+@Composable//use annotation to make a func composable
+private fun FirstComposableFunc(){
+    Text("Bismillah")
 }
