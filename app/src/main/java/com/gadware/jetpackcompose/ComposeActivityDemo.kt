1d7d46b682afc5e9e0795a90a9fa743afa315b42
Saawry <saawry.hb@gmail.com>
2025-09-11 16:12:37 +0600

    Modified user info card  with Modifier and Spacer

--- app/src/main/java/com/gadware/jetpackcompose/ComposeActivityDemo.kt
+++ app/src/main/java/com/gadware/jetpackcompose/ComposeActivityDemo.kt
@@ -6,10 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.nio.file.WatchEvent
 
 class ComposeActivityDemo : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +32,18 @@ class ComposeActivityDemo : ComponentActivity() {
 
 @Composable
 fun DisplayUserDetails(userInfo: UserInfo){
-    Row {
+    Row(modifier = Modifier.padding(5.dp)) {//// Add padding around our message
         Image(
             painter = painterResource(R.drawable.ic_launcher_foreground),
-            contentDescription = "This Is Profile Picture"
+            contentDescription = "This Is Profile Picture",
+            modifier = Modifier.size(40.dp)//// Set image size to 40 dp
+                .clip(CircleShape)//// Clip image to be shaped as a circle
         )
+
+        Spacer(modifier = Modifier.width(5.dp))//// Add a horizontal space between the image and the column
         Column {
             Text( "Name: ${userInfo.name}")
+            Spacer(modifier = Modifier.height(4.dp))//// Add a vertical space between the name and address texts
             Text( "Address: ${userInfo.address}")
         }
     }
